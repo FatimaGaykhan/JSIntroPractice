ed8c722c1a0251460c13f686dadee91679d2a90c
FatimaGaykhan <fatimaeg@code.edu.az>
2024-04-24 21:35:01 +0400

    Added Js Intro Practice

---
+++ assets/js/script.js
@@ -0,0 +1,164 @@
+// "use strict";
+
+var address = "28 May";
+
+var address = "Genclik";
+
+let phone = 6543213;
+
+const email = "test@gmail.com";
+
+let a = 5;
+let b = 10;
+
+let result = a + b;
+
+a++;
+
+// console.log(result);
+// console.log(a);
+
+
+let isMarried = true;
+
+
+// if (isMarried) {
+//     console.log("Evlidir");
+// }
+// else {
+//     console.log("Subaydir");
+// }
+
+
+let number = 100;
+
+// if (number > 50 && number < 150) {
+//     console.log("yes");
+// }
+// else {
+//     console.log("no");
+// }
+
+
+let weekDay = "Wednesday";
+
+// switch (weekDay) {
+//     case "Tuesday" :
+//         console.log("Tuesday")
+//         break;
+//     case "Wednesday":
+//         console.log("Wednesday");
+//         break;
+//     default:
+//         console.log("Not selected");
+//         break;
+// }
+
+
+// alert("Welcome");
+
+// confirm("Test");
+
+// let person = prompt("Test");
+
+// if(person == null){
+//     console.log("Operation cancelled");
+// }
+// else{
+//     console.log(person);
+// }
+
+let c = 5
+
+// while(c < 8){
+//     c++;
+//     console.log(c)
+// }
+
+
+
+
+// for (let i = 0; i < 50; i++) {
+//     console.log(i);
+// }
+
+
+
+// let k = 5;
+// let m = k**3;
+// console.log(m);
+
+
+
+
+let num = 100;
+
+let test = "100";
+
+// console.log(num === test);
+
+
+
+let datas = [1,2,3,"salam",true];
+
+// for (let i = 0; i < datas.length; i++) {
+//     if(!isNaN(Number(datas[i]))){
+//         console.log(datas[i]);
+//     }
+// }
+
+
+// for (let i = 0; i < datas.length; i++) {
+//     if(typeof datas[i] == "number"){
+//         console.log(datas[i]);
+//     }
+// }
+
+
+
+let elem = 50;
+
+let result2 = elem > 100 ? "Yes" : "No";
+// console.log(result2);
+
+
+
+let q;
+let w = null;
+
+
+let elements = [30,5,6,8,9,13,23];
+
+let sum = 0;
+
+for (let i = 0; i < elements.length; i++) {
+    sum+=elements[i];   
+}
+// console.log(sum);
+
+
+
+
+let isTrue = true;
+
+if(isTrue){
+    let test1 = "Test2";
+    if(true){
+        let surname = "Test";
+    } 
+}
+
+// console.log(surname);
+
+
+const products = ["test1","test2"];
+
+for (let i = 0; i < products.length; i++) {
+    console.log(products[i]);
+}
+
+const products2 = ["test1","test2"];
+
+products[0] = "test100";
+
+products = products2;
